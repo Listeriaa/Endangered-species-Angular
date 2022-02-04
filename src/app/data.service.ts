e70be6079354a0b0e96d8d616d4731a463676aeb
Listeriaa <laeti.rd@gmail.com>
2022-02-04 09:49:33 +0100

    delete environnments from stage

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -10,9 +10,9 @@ import {Specie} from './specie'
 export class DataService {
 
   constructor(private http : HttpClient) { }
-
+  
   //private apiUrl = "https://api.endangered-species.laetitia-dev.com/api/v1"
-  private apiUrl = "http://localhost:8000/api/v1"
+  private apiUrl = "https://apiv3.iucnredlist.org/api/v3/"
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
@@ -30,12 +30,13 @@ export class DataService {
 
   getList(): Observable<Specie[]> {
 
-    const url = `${this.apiUrl}/list`
-    const list = this.http.get<Specie[]>(url)
-    
-    return this.http.get<Specie[]>(url)
+    const url = `${this.apiUrl}country/getspecies/FR?token=${environment.apiKey}`
+    //const url = `${this.apiUrl}/list`
+    const list = this.http.get(url)
+    console.log(list)
+    return this.http.get<any>(url)
     .pipe(
-      
+      map(response => response.result),
       catchError(this.handleError('getList', [])))
   }
 
